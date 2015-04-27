2d0b3e9bd3ce860486d92133004fdfb82db0fe56
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-27 16:52:34 -0400

    get scatter working, can't use seemingly official nvd3:nvd3 package as of now

--- scatter1/scatter1.js
+++ scatter1/scatter1.js
@@ -1,66 +1,57 @@
-var Scatter = new Meteor.Collection('scatter');
+Scatter = new Meteor.Collection("Scatter");
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 if (Meteor.isClient) {
-  Template.chart.rendered = function () {
+  Template.chart.rendered = function() {
+    var chart = nv.models.lineChart()
+      .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+      .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+      .showYAxis(true)        //Show the y-axis
+      .showXAxis(true)        //Show the x-axis
+    ;
 
-      nv.utils.symbolMap.set('thin-x', function (size) {
-          size = Math.sqrt(size);
-          return 'M' + (-size/2) + ',' + (-size/2) +
-        'l' + size + ',' + size +
-        'm0,' + -(size) +
-        'l' + (-size) + ',' + size;
-      });
+    nv.addGraph(function() {
+      chart.xAxis.axisLabel('Time').tickFormat(d3.format('d'));
+      chart.yAxis.axisLabel('Conc (ng/mL)').tickFormat(d3.format('d'));
+      d3.select('#chart svg').datum(
+        [{ values: Scatter.find().fetch()}]
+      ).call(chart);
+      nv.utils.windowResize(function() { chart.update(); });
+      return chart;
+    });
 
+    this.autorun(function () {
+      d3.select('#chart svg').datum(
+        [{ values: Scatter.find().fetch(), key: 'ID' }]
+      ).call(chart);
+      chart.update();
+    });
+  };
 
-      var chart;
-      nv.addGraph(function () {
-          chart = nv.models.scatterChart()
-          .showDistX(true)
-          .showDistY(true)
-          .useVoronoi(true)
-          .color(d3.scale.category10().range())
-          .duration(300);
-
-          chart.dispatch.on('renderEnd', function() {
-            console.log('render complete');
-          });
-
-          chart.xAxis.tickFormat(d3.format('.02f'));
-          chart.yAxis.tickFormat(d3.format('.02f'));
-
-          d3.select('#scatter svg')
-          .datum(randomData(4, 40))
-          .call(chart);
-
-          nv.utils.windowResize(chart.update);
-          chart.dispatch.on('stateChange', function(e) { ('New State:', JSON.stringify(e)); });
-          return chart;
-      });
-      function randomData(groups, points) { //# groups,# points per group
-          // smiley and thin-x are our custom symbols!
-          var data = [],
-              shapes = ['thin-x', 'circle', 'cross', 'triangle-up', 'triangle-down', 'diamond', 'square'],
-              random = d3.random.normal();
-          for (i = 0; i < groups; i++) {
-              data.push({
-                  key: 'Group ' + i,
-                  values: []
-              });
-              for (j = 0; j < points; j++) {
-                  data[i].values.push({
-                      x: random(),
-                      y: random(),
-                      size: Math.round(Math.random() * 100) / 100,
-                      shape: shapes[j % shapes.length]
-                  });
-              }
-          }
-          return data;
+  Template.chart.events({
+    'click #addDataButton': function() {
+      var point = getRandomInt(13, 89);
+      var lastPoint = Scatter.findOne({}, {fields:{x:1},sort:{x:-1},limit:1,reactive:false});
+      if (lastPoint) {
+        Scatter.insert({x:(lastPoint.x + 1), y:point});
+      } else {
+        Scatter.insert({x:1, y:point});
       }
-};
+    },
+    'click #removeDataButton': function() {
+      var lastPoint = Scatter.findOne({}, {fields:{x:1},sort:{x:-1},limit:1,reactive:false});
+      if (lastPoint) {
+        Scatter.remove(lastPoint._id);
+      }
+    }
+  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
-});
+  });
 }
