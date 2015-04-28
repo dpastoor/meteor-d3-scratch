e67cefbbc5d28ec13f180f6603e40b93c04bef0f
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-28 18:48:21 -0400

    render from database but doesn't update yet if database updated

--- conctimedb/conctime.js
+++ conctimedb/conctime.js
@@ -1,4 +1,4 @@
-Scatter = new Meteor.Collection("Scatter");
+  Data = new Meteor.Collection("Data");
 
 if (Meteor.isClient) {
   Template.chart.rendered = function() {
@@ -8,6 +8,8 @@ if (Meteor.isClient) {
   var width = 600;
   var height = 300;
   var zoom = 1;
+  var data = Data.findOne().data;
+
   chart.useInteractiveGuideline(true);
   chart.xAxis
       .axisLabel('Time (hours)')
@@ -22,7 +24,7 @@ if (Meteor.isClient) {
       .attr('perserveAspectRatio', 'xMinYMid')
       .attr('width', width)
       .attr('height', height)
-      .datum(concTime());
+      .datum(data);
   setChartViewBox();
   resizeChart();
   nv.utils.windowResize(resizeChart);
@@ -71,37 +73,34 @@ if (Meteor.isClient) {
 });
 
 
-function concTime() {
-
-  return [
-      {
-          values: [{x:0,y:0},{x:0.25,y:8.61},{x:0.5,y:19.44},{x:1,y:34.01},{x:2,y:30.23},{x:3,y:31.3},
-          {x:4,y:24.98},{x:6,y:23.38},{x:8,y:23.51},{x:12,y:14.68},{x:16,y:9.07},{x:24,y:5.3}] ,
-          key: "1",
-          color: "#ff7f0e"
-      },
-      {
-          values:[{"x":0,"y":0},{"x":0.25,"y":30.4},{"x":0.5,"y":47.63},{"x":1,"y":64.43},{"x":2,"y":100.18},{"x":3,"y":92.56},{"x":4,"y":65.27},
-          {"x":6,"y":49.26},{"x":8,"y":60.29},{"x":12,"y":44.25},{"x":16,"y":41.85},{"x":24,"y":32.9}] ,
-          key: "2",
-          color: "#2ca02c"
-      },
-      {
-          values:[{"x":0,"y":0},{"x":0.25,"y":31.4},{"x":0.5,"y":42.63},{"x":1,"y":69.43},{"x":2,"y":91.18},{"x":3,"y":84.56},{"x":4,"y":66.27},
-          {"x":6,"y":52.26},{"x":8,"y":62.29},{"x":12,"y":41.25},{"x":16,"y":38.85},{"x":24,"y":31.9}] ,
-          key: "3",
-          color: "#4ecdc4"
-      }
-  ];
-}
-
-
-
-
 };
 }
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if (Data.find().count() ===0) {
+      Data.insert({data: [
+          {
+              values: [{x:0,y:0},{x:0.25,y:8.61},{x:0.5,y:19.44},{x:1,y:34.01},{x:2,y:30.23},{x:3,y:31.3},
+              {x:4,y:24.98},{x:6,y:23.38},{x:8,y:23.51},{x:12,y:14.68},{x:16,y:9.07},{x:24,y:5.3}] ,
+              key: "1",
+              color: "#ff7f0e"
+          },
+          {
+              values:[{"x":0,"y":0},{"x":0.25,"y":30.4},{"x":0.5,"y":47.63},{"x":1,"y":64.43},{"x":2,"y":100.18},{"x":3,"y":92.56},{"x":4,"y":65.27},
+              {"x":6,"y":49.26},{"x":8,"y":60.29},{"x":12,"y":44.25},{"x":16,"y":41.85},{"x":24,"y":32.9}] ,
+              key: "2",
+              color: "#2ca02c"
+          },
+          {
+              values:[{"x":0,"y":0},{"x":0.25,"y":31.4},{"x":0.5,"y":42.63},{"x":1,"y":69.43},{"x":2,"y":91.18},{"x":3,"y":84.56},{"x":4,"y":66.27},
+              {"x":6,"y":52.26},{"x":8,"y":62.29},{"x":12,"y":41.25},{"x":16,"y":38.85},{"x":24,"y":31.9}] ,
+              key: "3",
+              color: "#4ecdc4"
+          }
+        ]
+
+      });
+    }
   });
 }
