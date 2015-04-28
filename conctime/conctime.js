2488f71f1804372b72d19eafc37e204042d67e39
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-28 13:01:38 -0400

    get conc time working for 2 ID's hard coded in

--- conctime/conctime.js
+++ conctime/conctime.js
@@ -10,18 +10,19 @@ if (Meteor.isClient) {
   var zoom = 1;
   chart.useInteractiveGuideline(true);
   chart.xAxis
+      .axisLabel('Time (hours)')
       .tickFormat(d3.format(',r'));
   chart.lines.dispatch.on("elementClick", function(evt) {
       console.log(evt);
   });
   chart.yAxis
-      .axisLabel('Voltage (v)')
+      .axisLabel('Concentration (ng/mL)')
       .tickFormat(d3.format(',.2f'));
   d3.select('#chart svg')
       .attr('perserveAspectRatio', 'xMinYMid')
       .attr('width', width)
       .attr('height', height)
-      .datum(sinAndCos());
+      .datum(concTime());
   setChartViewBox();
   resizeChart();
   nv.utils.windowResize(resizeChart);
@@ -68,26 +69,27 @@ if (Meteor.isClient) {
   }
   return chart;
 });
-function sinAndCos() {
-  var sin = [],
-      cos = [];
-  for (var i = 0; i < 100; i++) {
-      sin.push({x: i, y: Math.sin(i/10) });
-      cos.push({x: i, y: .5 * Math.cos(i/10)});
-  }
+
+
+function concTime() {
+
   return [
       {
-          values: sin,
-          key: "Sine Wave",
+          values: [{x:0,y:0},{x:0.25,y:8.61},{x:0.5,y:19.44},{x:1,y:34.01},{x:2,y:30.23},{x:3,y:31.3},
+          {x:4,y:24.98},{x:6,y:23.38},{x:8,y:23.51},{x:12,y:14.68},{x:16,y:9.07},{x:24,y:5.3}] ,
+          key: "1",
           color: "#ff7f0e"
       },
       {
-          values: cos,
-          key: "Cosine Wave",
+          values:[{"x":0,"y":0},{"x":0.25,"y":30.4},{"x":0.5,"y":47.63},{"x":1,"y":64.43},{"x":2,"y":100.18},{"x":3,"y":92.56},{"x":4,"y":65.27},
+          {"x":6,"y":49.26},{"x":8,"y":60.29},{"x":12,"y":44.25},{"x":16,"y":41.85},{"x":24,"y":32.9}] ,
+          key: "2",
           color: "#2ca02c"
       }
   ];
 }
+
+
 
 
 };
