00ead2203b5abb9165543fd460257be776046ef0
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-29 11:45:56 -0400

    update to put into tracker plus in renderChart() function, still not updating when update database

--- conctimedb/conctime.js
+++ conctimedb/conctime.js
@@ -2,78 +2,94 @@
 
 if (Meteor.isClient) {
   Template.chart.rendered = function() {
-    nv.addGraph(function() {
-  var chart = nv.models.lineChart();
-  var fitScreen = false;
-  var width = 600;
-  var height = 300;
-  var zoom = 1;
-  var data = Data.findOne().data;
 
-  chart.useInteractiveGuideline(true);
-  chart.xAxis
-      .axisLabel('Time (hours)')
-      .tickFormat(d3.format(',r'));
-  chart.lines.dispatch.on("elementClick", function(evt) {
-      console.log(evt);
+  Tracker.autorun(function() {
+    console.log('running again!');
+    renderChart();
   });
-  chart.yAxis
-      .axisLabel('Concentration (ng/mL)')
-      .tickFormat(d3.format(',.2f'));
-  d3.select('#chart svg')
-      .attr('perserveAspectRatio', 'xMinYMid')
-      .attr('width', width)
-      .attr('height', height)
-      .datum(data);
-  setChartViewBox();
-  resizeChart();
-  nv.utils.windowResize(resizeChart);
-  d3.select('#zoomIn').on('click', zoomIn);
-  d3.select('#zoomOut').on('click', zoomOut);
-  function setChartViewBox() {
-      var w = width * zoom,
-          h = height * zoom;
-      chart
-          .width(w)
-          .height(h);
-      d3.select('#chart svg')
-          .attr('viewBox', '0 0 ' + w + ' ' + h)
-          .transition().duration(500)
-          .call(chart);
-  }
-  function zoomOut() {
-      zoom += .25;
-      setChartViewBox();
-  }
-  function zoomIn() {
-      if (zoom <= .5) return;
-      zoom -= .25;
-      setChartViewBox();
-  }
-  // This resize simply sets the SVG's dimensions, without a need to recall the chart code
-  // Resizing because of the viewbox and perserveAspectRatio settings
-  // This scales the interior of the chart unlike the above
-  function resizeChart() {
-      var container = d3.select('#chart1');
-      var svg = container.select('svg');
-      if (fitScreen) {
-          // resize based on container's width AND HEIGHT
-          var windowSize = nv.utils.windowSize();
-          svg.attr("width", windowSize.width);
-          svg.attr("height", windowSize.height);
-      } else {
-          // resize based on container's width
-          var aspect = chart.width() / chart.height();
-          var targetWidth = parseInt(container.style('width'));
-          svg.attr("width", targetWidth);
-          svg.attr("height", Math.round(targetWidth / aspect));
-      }
-  }
-  return chart;
-});
+  function renderChart() {
 
+    nv.addGraph(function() {
+    var chart = nv.models.lineChart();
+    var fitScreen = false;
+    var width = 600;
+    var height = 300;
+    var zoom = 1;
+    // currently a bug where if new data is added and refreshed get
+    //property data undefined and by just adding a space etc here resaving so meteor
+    // updates the chart will re-render properly
+
+      var data = Data.findOne().data;
+
+
+
+      chart.useInteractiveGuideline(true);
+      chart.xAxis
+          .axisLabel('Time (hours)')
+          .tickFormat(d3.format(',r'));
+      chart.lines.dispatch.on("elementClick", function(evt) {
+          console.log(evt);
+      });
+      chart.yAxis
+          .axisLabel('Concentration (ng/mL)')
+          .tickFormat(d3.format(',.2f'));
+      d3.select('#chart svg')
+          .attr('perserveAspectRatio', 'xMinYMid')
+          .attr('width', width)
+          .attr('height', height)
+          .datum(data);
+      setChartViewBox();
+      resizeChart();
+      nv.utils.windowResize(resizeChart);
+      d3.select('#zoomIn').on('click', zoomIn);
+      d3.select('#zoomOut').on('click', zoomOut);
+      function setChartViewBox() {
+          var w = width * zoom,
+              h = height * zoom;
+          chart
+              .width(w)
+              .height(h);
+          d3.select('#chart svg')
+              .attr('viewBox', '0 0 ' + w + ' ' + h)
+              .transition().duration(500)
+              .call(chart);
+      }
+      function zoomOut() {
+          zoom += .25;
+          setChartViewBox();
+      }
+      function zoomIn() {
+          if (zoom <= .5) return;
+          zoom -= .25;
+          setChartViewBox();
+      }
+      // This resize simply sets the SVG's dimensions, without a need to recall the chart code
+      // Resizing because of the viewbox and perserveAspectRatio settings
+      // This scales the interior of the chart unlike the above
+      function resizeChart() {
+          var container = d3.select('#chart1');
+          var svg = container.select('svg');
+          if (fitScreen) {
+              // resize based on container's width AND HEIGHT
+              var windowSize = nv.utils.windowSize();
+              svg.attr("width", windowSize.width);
+              svg.attr("height", windowSize.height);
+          } else {
+              // resize based on container's width
+              var aspect = chart.width() / chart.height();
+              var targetWidth = parseInt(container.style('width'));
+              svg.attr("width", targetWidth);
+              svg.attr("height", Math.round(targetWidth / aspect));
+          }
+      }
+      return chart;
+    });
+
+  }
 
 };
+
+
 }
 if (Meteor.isServer) {
   Meteor.startup(function () {
